c467c4d68df5fbdd0ef583ce185d8a8614c1c0c8
TonyOgg <tony9808@mail.ru>
2020-09-15 22:00:46 +0300

    Project_1

--- App1/models.py
+++ App1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Regions(models.Model):
+    index = models.IntegerField()
+    name = models.CharField(max_length=15)
+
+class Cities(models.Model):
+    cname = models.CharField(max_length=45)
+    region = models.ForeignKey(Regions, on_delete=models.CASCADE)
+
