c91314e0bd3a07c1e0ae4ce9d326588aef7cd969
Dhoiney <dhoiney@gmail.com>
2020-09-17 18:52:52 +0300

    Django project

--- App1/models.py
+++ App1/models.py
@@ -6,7 +6,24 @@ class Regions(models.Model):
     index = models.IntegerField()
     name = models.CharField(max_length=15)
 
+    def __str__(self):
+        return self.name
+
 class Cities(models.Model):
     cname = models.CharField(max_length=45)
     region = models.ForeignKey(Regions, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.cname
+
+class Attractions(models.Model):
+    names = models.CharField(max_length=50)
+    city = models.OneToOneField(Cities, on_delete=models.CASCADE)
+    category = models.CharField(max_length=30)
+    description = models.TextField()
+    dateoffoundation = models.DateField()
+
+    def __str__(self):
+        return self.names
+
 
